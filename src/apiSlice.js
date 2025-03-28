e3b329007aa961d3d9ed1cf9d6d1b37f43d46200
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-03-28 18:16:42 +0530

    Completed the Project

---
+++ src/apiSlice.js
@@ -0,0 +1,85 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  tagTypes: ["Tasks"],
+  endpoints: (builder) => ({
+    getTasks: builder.query({
+      query: () => "/tasks",
+      transformResponse: (tasks) => tasks.reverse(),
+      providesTags: ["Tasks"],
+    }),
+    addTask: builder.mutation({
+      query: (task) => ({
+        url: "/tasks",
+        method: "POST",
+        body: task,
+      }),
+      invalidatesTags: ["Tasks"],
+      async onQueryStarted(task, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          api.util.updateQueryData("getTasks", undefined, (draft) => {
+            draft.unshift({ id: crypto.randomUUID(), ...task })
+          })
+        )
+
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
+    }),
+    updateTask: builder.mutation({
+      query: ({ id, ...task }) => ({
+        url: `/tasks/${id}`,
+        method: "PATCH",
+        body: task,
+      }),
+      invalidatesTags: ["Tasks"],
+      async onQueryStarted({ id, ...task }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          api.util.updateQueryData("getTasks", undefined, (draft) => {
+            const taskIndex = draft.findIndex((task) => task.id === id)
+            draft[taskIndex] = { ...draft[taskIndex], ...task }
+          })
+        )
+
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
+    }),
+    deleteTask: builder.mutation({
+      query: (id) => ({
+        url: `/tasks/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Tasks"],
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          api.util.updateQueryData("getTasks", undefined, (draft) => {
+            const taskIndex = draft.findIndex((task) => task.id === id)
+            draft.splice(taskIndex, 1)
+          })
+        )
+
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
+    }),
+  }),
+})
+
+export const {
+  useGetTasksQuery,
+  useAddTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} = api
